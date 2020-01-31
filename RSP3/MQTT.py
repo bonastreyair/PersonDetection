01f98b8b443c15897f6a62353232732d815f686d
Yair <37800834+bonastreyair@users.noreply.github.com>
2020-01-31 16:00:04 +0100

    Update MQTT.py
    aviam que et semblen els canvis i digam si va be :)

--- RSP3/MQTT.py
+++ RSP3/MQTT.py
@@ -16,33 +16,49 @@ TOPIC_PIR = "RSP3/PIR/Presencia"
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIR_PIN, GPIO.IN)
 
-while True:
-    client = mqtt.Client()
-    client.connect(BROKER,1883,60)
+mqttc = mqtt.Client()
+
+def on_connect(mqttc, obj, flags, rc):
+    print("rc: " + str(rc))
     
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR,DHT_PIN)
+def on_disconnect(mqttc, obj, flags, rc):
+    mqttc.connect(BROKER, 1883, 60)
+    print("reconnecting...")
+
+def on_publish(mqttc, obj, mid):
+    print("mid: " + str(mid))
+    
+
+mqttc.on_connect = on_connect
+mqttc.on_disconnect = on_disconnect
+mqttc.on_publish = on_publish
+
+mqttc.connect(BROKER, 1883, 60)
+
+mqttc.loop_start()
+    
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 
     if humidity is not None:
-        MENSAJE_HUMI = "{1:0.1f}".format(temperature,humidity)
-        client.publish(TOPIC_HUMI, MENSAJE_HUMI) 
-        print("Humidity={1:0.1f}".format(temperature,humidity))
-        time.sleep(1) 
+        MENSAJE_HUMI = "{1:0.1f}".format(temperature, humidity)
+        mqttc.publish(TOPIC_HUMI, MENSAJE_HUMI) 
+        print("Humidity={1:0.1f}".format(temperature, humidity))
     else:
         print("Failed to get reading Humidity")
 
     if temperature is not None:
-        MENSAJE_TEMP = "{0:0.1f}".format(temperature,humidity)
-        client.publish(TOPIC_TEMP, MENSAJE_TEMP)
-        print("Temperature={0:0.1f}".format(temperature,humidity))
-        time.sleep(1)
+        MENSAJE_TEMP = "{0:0.1f}".format(temperature, humidity)
+        mqttc.publish(TOPIC_TEMP, MENSAJE_TEMP)
+        print("Temperature={0:0.1f}".format(temperature, humidity))
     else:
         print("Failed to get reading Temperature")
         
-    if GPIO.input(PIR_PIN) == True:
+    if GPIO.input(PIR_PIN):
         print("Detectando")
-        client.publish(TOPIC_PIR,"1")
+        client.publish(TOPIC_PIR, "1")
     else:
         print("No Detectando")
-        client.publish(TOPIC_PIR,"0")
-
-
+        client.publish(TOPIC_PIR, "0")
+        
+    time.sleep(1)
